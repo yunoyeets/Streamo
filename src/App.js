5294cbd9e48d65eed98494ee7d27ccd6826f1be1
Neha Pandya <73463081+yunoyeets@users.noreply.github.com>
2022-06-01 15:41:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import {useState, useEffect} from 'react';
+
+import SearchIcon from './search.svg'
+import MovieCard from './MovieCart';
+import "./App.css";
+
+const API_URL = 'http://www.omdbapi.com?apikey=9e1a7bb2';
+
+const App = () => { 
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
+        searchMovies('Italy');
+    }, []);
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }; 
+    
+
+    return(
+        <div className="app">
+            <h1>StreamO</h1>
+
+            <div className = "search">
+                <input 
+                 placeholder= "Search for movies"
+                 value= {searchTerm}
+                 onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img src = {SearchIcon}
+                alt="Search"
+                onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+            
+                {movies?.length > 0 ? (
+                        <div className="container">
+                            {movies.map((movie) => (
+                                <MovieCard movie={movie}/>
+                            ))}
+                            <MovieCard movie = {movies[0]} />
+                        </div>
+                    ) : (
+                        <div className="empty">
+                            <h2>No movies found</h2>
+                        </div>
+                    )
+                }
+            
+        </div>
+    );
+}
+
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// 9e1a7bb2
+
+
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root')); 
